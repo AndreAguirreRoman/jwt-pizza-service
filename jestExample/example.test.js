55cb558aeef4b9cadb0a919cdb35405db278a300
AndreAguirreRoman <ankar_aguirre@live.com>
2026-02-01 22:13:38 -0700

    Deleted the jestexample from this project. Added the jest tests, and simple helloworld test

--- jestExample/example.test.js
+++
@@ -1,9 +0,0 @@
-const { add } = require('./add');
-
-test('add two numbers', () => {
-  expect(add(1, 1)).toBe(2);
-});
-
-test('add multiple numbers', () => {
-  expect(add(1, 2, 3, 4, 5)).toBe(15);
-});
