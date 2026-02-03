2f827c95565870ec9fab47542013e722896fd6cd
AndreAguirreRoman <ankar_aguirre@live.com>
2026-02-03 13:15:34 -0700

    Added the delete test for franchises with and without admin creds

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,11 +2,84 @@ const request = require('supertest');
 const app = require('../service');
 
 const admin = {email: 'a@jwt.com', password: 'admin'};
+const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 
+let adminToken;
 let token;
 
+let franchiseeEmail;
+let franchiseId;
+let storeId;
+
 beforeAll(async () => {
+
+    user.email = Math.random().toString(36).substring(2,12) + '@test.com';
+    const registerRes = await request(app).post('/api/auth').send(user);
+    token = registerRes.body.token;
+    expect(token).toBeDefined();
+
     const adminLoginRes = await request(app).put('/api/auth').send(admin);
     adminToken = adminLoginRes.body.token;
     expect(adminToken).toBeDefined();
+
+    const franchiseRes = await request(app)
+    .post('/api/franchise')
+    .set('Authorization', `Bearer ${adminToken}`)
+    .send({ name: `fr-${Date.now()}`, admins: [{ email: 'a@jwt.com' }] });
+
+    franchiseId = franchiseRes.body.id;
+    expect(franchiseId).toBeDefined();
+
+    const storeRes = await request(app)
+        .post(`/api/franchise/${franchiseId}/store`)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send({ name: `store-${Date.now()}` });
+
+    storeId = storeRes.body.id;
+    expect(storeId).toBeDefined();
+
+    franchiseeEmail = `f-${Date.now()}@test.com`;
+    const franchisee = { name: 'pizza franchisee', email: franchiseeEmail, password: 'a' };
+    const regRes = await request(app).post('/api/auth').send(franchisee);
+    expect(regRes.status).toBe(200);
 })
+
+test("POST new franchise", async() => {
+    const franchiseName = `pizzaPocket~${Date.now()+' franchise'}`;
+
+    const res = await request(app).post('/api/franchise')
+        .set('Authorization', `Bearer ${adminToken}`).send({
+            name: franchiseName,
+            admins: [{email: franchiseeEmail}]
+        });
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('name', franchiseName);
+})
+
+test("GET franchises", async () =>{
+    const res = await request(app)
+        .get('/api/franchise')
+        .query({ page: 0, limit: 10, name: '*' });
+    
+    expect(res.status).toBe(200);
+
+})
+
+test('DELETE store non-admin', async () => {
+  const res = await request(app)
+    .delete(`/api/franchise/${franchiseId}/store/${storeId}`)
+    .set('Authorization', `Bearer ${token}`);
+
+  expect(res.status).toBe(403);
+});
+
+test('DELETE store admin', async () => {
+  const res = await request(app)
+    .delete(`/api/franchise/${franchiseId}/store/${storeId}`)
+    .set('Authorization', `Bearer ${adminToken}`);
+
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({message: 'store deleted'});
+});
