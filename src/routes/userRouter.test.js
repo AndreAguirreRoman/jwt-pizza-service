d212b2d11a5c292e684dd97679599fc434a59991
AndreAguirreRoman <ankar_aguirre@live.com>
2026-04-10 12:14:50 -0600

    Fixed error to display error 403 now in tests

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -52,7 +52,7 @@ test('list users', async () => {
   const listUsersRes = await request(app)
     .get('/api/user')
     .set('Authorization', 'Bearer ' + userToken);
-  expect(listUsersRes.status).toBe(500);
+  expect(listUsersRes.status).toBe(403);
 });
 
 async function registerUser(service) {
